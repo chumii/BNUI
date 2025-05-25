35c4fb0412a6ee2f314013f5c6d1e7f79f925e08
chumii <jonas.dreier@gmail.com>
2025-05-25 16:08:02 +0200

    slash command template

---
+++ Core/Commands.lua
@@ -0,0 +1,25 @@
+local M, C, L = BNUI[1], BNUI[2], BNUI[3]
+
+-- Open Command Window with /kkhelp
+local function OpenTestCommand()
+	M:Print("Test Command")
+end
+
+-- Command Mapping Table
+local commandMap = {	
+	test = OpenTestCommand,
+	-- Add more commands as needed...
+}
+
+-- Slash Command Handler
+SlashCmdList["BNUI"] = function(input)
+	local command, args = strsplit(" ", input, 2)
+	command = string.lower(command)
+
+	if commandMap[command] then
+		commandMap[command](args)
+	else
+		M:Print("Unknown command: " .. command)
+	end
+end
+_G.SLASH_BNUI1 = "/bn"
