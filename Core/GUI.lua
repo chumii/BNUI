bd553a28d2c28e8bf11b51b185b20a9933dfc74b
chumii <jonas.dreier@gmail.com>
2025-06-07 04:13:58 +0200

    remove test categories

--- Core/GUI.lua
+++ Core/GUI.lua
@@ -680,11 +680,6 @@ function GUI.Enable(self)
     end
     self:AddCategory("General", {"Profiles", "Settings"})
     self:AddCategory("Unitframes", {"Player", "Target", "Focus", "Pet"})
-    self:AddCategory("Test", {"Test 1", "Test 2", "Test 3", "Test 4"})
-    self:AddCategory("Test 5", {"Test 6", "Test 7", "Test 8", "Test 9"})
-    self:AddCategory("Test 10", {"Test 11", "Test 12", "Test 13", "Test 14", "Test xx", "Test yy"})
-    self:AddCategory("Test 15", {"Test 16", "Test 17", "Test 18", "Test 19"})
-    self:AddCategory("Test 20", {"Test 21", "Test 22", "Test 23", "Test 24"})
 
     -- Configure GUI elements from external file
     self:InitializeConfigElements()
